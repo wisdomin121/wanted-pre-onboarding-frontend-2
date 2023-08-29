ffba3b6625ceff141c92bd0046c92610eed7b519
wisdomin121@naver.com <wisdomin121@naver.com>
2023-08-29 21:48:03 +0900

    style: 전체 레이아웃 모바일 크기로 설정
    - 모바일 화면으로 레이아웃 설정했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { styled } from 'styled-components'
+
+import { PageRouter } from 'pages'
+
 function App() {
-  return <div></div>
+  return (
+    <CommonLayout>
+      <PageRouter />
+    </CommonLayout>
+  )
 }
+
+const CommonLayout = styled.div`
+  width: 100%;
+  background-color: pink;
+
+  @media screen and (min-width: 426px) {
+    max-width: 425px;
+    margin-left: calc((100% - 425px) / 2);
+  }
+`
 
 export default App
