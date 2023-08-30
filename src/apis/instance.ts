a87c4cb5dd6084cccfdb612a1cb6db6d4361acdd
wisdomin121@naver.com <wisdomin121@naver.com>
2023-08-31 00:52:14 +0900

    fix: 버셀 오류 해결중 2

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,11 +1,10 @@
 import axios from 'axios'
-import { BASE_URL } from 'data/const'
 import { useNavigate } from 'react-router-dom'
 
 const navigate = useNavigate()
 
 export const API = axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'https://api.github.com/repos/',
   headers: {
     'Content-Type': 'application/vnd.github+json',
     Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}`,
